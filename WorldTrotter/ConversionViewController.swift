3edeeccd301df42a948b3d799891d993252a71a5
tizz98 <dev.tizz98@gmail.com>
2016-03-26 00:45:21 -0400

    end chp 4

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Elijah on 3/26/16.
+//  Copyright © 2016 Zumh. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+
+        return !(existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil)
+    }
+
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5 / 9)
+        } else {
+            return nil
+        }
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+}
