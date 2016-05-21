6c7bce25e1862e6258c84a466dd6fef7a76260ff
Elijah Wilson <dev.tizz98@gmail.com>
2016-05-21 13:39:01 -0700

    tabs & map view

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -12,6 +12,39 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("ConversionViewController loaded its view")
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if isNighttime() {
+            self.view.backgroundColor = UIColor.darkGrayColor()
+        } else {
+            self.view.backgroundColor = UIColorFromHex(0xF5F4F1)
+        }
+    }
+
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+
+    func isNighttime() -> Bool {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Hour], fromDate: date)
+        let hour = components.hour
+
+        return hour < 6 || hour > 19
+    }
+
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
         let replacementTextHasDecimalSeparator = string.rangeOfString(".")
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Elijah on 5/21/16.
+//  Copyright © 2016 Zumh. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("MapViewController loaded its view")
+    }
+}
